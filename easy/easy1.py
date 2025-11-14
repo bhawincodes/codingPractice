0abea9442f9f7b2659818604fc53a590bf53c0ce
bhawinparkeria <bhawin.parkeria@gmail.com>
2025-11-14 10:11:47 +0530

    solved https://www.hackerearth.com/community/practice/make-palindrome-2-cf7b5be4/

---
+++ easy/easy1.py
@@ -0,0 +1,18 @@
+# https://www.hackerearth.com/community/practice/make-palindrome-2-cf7b5be4/
+
+t = int(input())
+for i in range(t):
+    len = int(input())
+    str = input()
+    dict = {}
+    cost = 0
+    for j in range(len):
+        if(str[j] in dict):
+            dict[str[j]] = dict[str[j]] + 1
+        else:
+            dict[str[j]] = 1
+    # checking if any off values present in the dictionary
+    for key, value in dict.items():
+        if value % 2 != 0:
+            cost = cost + 1
+    print(0 if cost == 0 else cost-1)
